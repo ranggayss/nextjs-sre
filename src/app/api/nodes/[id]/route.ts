bb86308df4d418f6567935fe0384c7b09d352497
ranggays <ranggayoviesaputra@gmail.com>
2025-07-11 11:43:22 +0700

    add GET endpoint node

--- src/app/api/nodes/[id]/route.ts
+++ src/app/api/nodes/[id]/route.ts
@@ -10,3 +10,25 @@ export async function DELETE(req: NextRequest, {params} : {
 
     return NextResponse.json({msg : 'Delete Node Succed', id: id}, {status: 201});
 };
+
+export async function GET(req: NextRequest, {params} : {params: Promise<{id: string}>}){
+    try {
+        const { id } = await params;
+
+        const node = await prisma.node.findUnique({
+            where: {
+                id: id,
+            },
+        });
+
+        if (!node){
+            throw new Error('There is no node from database');
+        }else{
+            return NextResponse.json(node);
+        }
+    } catch (error) {
+        console.error('Error', error);
+        return NextResponse.json({error: 'Error'}, {status: 500});
+    }
+
+}
