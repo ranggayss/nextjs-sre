ae894cdf0ffbff30fac6bb2a1e1fc342a67d20cd
ranggays <ranggayoviesaputra@gmail.com>
2025-07-15 21:36:59 +0700

    minor changes

--- src/app/api/annotation/route.ts
+++ src/app/api/annotation/route.ts
@@ -90,6 +90,7 @@ export async function GET(req: NextRequest){
                     select: {
                         id: true,
                         title: true,
+                        filePath: true,
                     }
                 }
             },
